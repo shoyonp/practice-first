379ac2df4734a226f83005e7c3dd9bd27d81396f
shoyonp <shoyonkumar143@gmail.com>
2024-08-29 15:47:34 +0600

    practice done

---
+++ first.js
@@ -0,0 +1,32 @@
+// for practice
+var tk = 1000;
+var orangePrice1kg = 400;
+var applePrice1kg = 300;
+var totalPrice = orangePrice1kg + applePrice1kg;
+console.log(tk - totalPrice)
+
+
+// -----------
+var math = 75.25;
+var bio = 65;
+var che = 80;
+var phy = 35.45;
+var bng = 99.50;
+var totalMark = math + bio + che + phy + bng;
+var avg = totalMark / 5;
+console.log(avg.toFixed(2))
+
+// ------------
+var input = 119;
+var divide = 5;
+var rem = input % divide;
+console.log(rem)
+
+
+// ----------
+const a = isNaN(2-10);
+const b = isNaN('11');
+const c = isNaN('ok');
+console.log(a)
+console.log(b)
+console.log(c)
