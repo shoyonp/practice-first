a78b483f5b09c972d93886c15b61779c2bd0f0f1
shoyonp <shoyonkumar143@gmail.com>
2024-08-30 15:13:11 +0600

    added  secondfile

---
+++ second.js
@@ -0,0 +1,10 @@
+// ----------------
+const burgerPrice = 500;
+const boughtBurger = 600;
+
+if(burgerPrice < boughtBurger){
+    console.log('free coke');
+}
+else{
+    console.log('coke 30 tk')
+}
