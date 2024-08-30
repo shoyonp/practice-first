478b7d0a35bb69b7fe1ca7fb572d1cf2c52fc8f8
shoyonp <shoyonkumar143@gmail.com>
2024-08-30 16:58:20 +0600

    nested not completed yet

--- second.js
+++ second.js
@@ -37,19 +37,59 @@ else{
 // ---------------------
 var marks = 76;
 if(marks >=90 && marks <=100){
-    console.log('A');
+    // console.log('A');
 }
 else if(marks >= 80 && marks <= 89){
-    console.log('B');
+    // console.log('B');
 }
 else if(marks >= 70 && marks <= 79){
-    console.log('C');
+    // console.log('C');
 }
 else if(marks >= 60 && marks <= 69){
-    console.log('D');
+    // console.log('D');
 }
 else{
     if(marks >= 0 && marks <= 59){
-        console.log('F')
+        // console.log('F')
     }
 }
+
+
+
+
+// -------------------------
+var myScore = 85;
+var friendScore = 75;
+if(myScore > 80){
+    console.log('inside friend score')
+}
+else{
+    if(friendScore > 80){
+        console.log('go for lunch')
+    }
+    else{
+        if(friendScore < 80 && friendScore >= 60){
+             console.log('good luck next time')
+        }
+        else{
+            if(friendScore < 60 && friendScore >=40){
+                console.log('message unseen')
+            }
+            else{
+                if(friendScore < 40){
+                    console.log('block your friend')
+                }
+                else{
+                    if(myScore < 80){
+                        console.log('go to home and sleep and act sad')
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+
+// --------------------------
